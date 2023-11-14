a1e758807a34985c9cc6719228b4485572e2970a
shigeyuki-nakano <thuyajino_in_the_groove@outlook.jp>
2023-11-15 02:55:18 +0900

    登録パスの修正

--- src/main/java/com/example/springboot/sample/with/s3/controller/RegisterController.java
+++ src/main/java/com/example/springboot/sample/with/s3/controller/RegisterController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/create")
+@RequestMapping("/register")
 @RequiredArgsConstructor
 public class RegisterController {
 
