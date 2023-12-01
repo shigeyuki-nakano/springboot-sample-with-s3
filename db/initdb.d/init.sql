556132b462164c9f8d32f2eb69f7d07e83f4d89e
shigeyuki-nakano <thuyajino_in_the_groove@outlook.jp>
2023-12-01 11:42:47 +0900

    テーブル定義修正

--- db/initdb.d/init.sql
+++ db/initdb.d/init.sql
@@ -1,11 +1,13 @@
-CREATE TABLE food_memo.memo (
-    id int primary key auto_increment,
-    title varchar(255),
+CREATE TABLE IF NOT EXISTS food_memo.memo
+(
+    id      int primary key auto_increment,
+    title   varchar(255),
     content varchar(500)
 );
 
-CREATE TABLE food_memo.image (
-    id int primary key auto_increment,
-    memo_id int NOT NULL,
-    path varchar(1000) NOT NULL
+CREATE TABLE IF NOT EXISTS food_memo.image
+(
+    id      int primary key auto_increment,
+    memo_id int           NOT NULL,
+    path    varchar(1000) NOT NULL
 );
