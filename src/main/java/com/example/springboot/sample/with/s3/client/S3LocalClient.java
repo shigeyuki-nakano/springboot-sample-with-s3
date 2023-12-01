cd2ec1f08104e6bede9cf539bf4314fa68856885
shigeyuki-nakano <thuyajino_in_the_groove@outlook.jp>
2023-12-01 12:18:39 +0900

    S3クライアントの本番・ローカル設定

--- src/main/java/com/example/springboot/sample/with/s3/client/S3LocalClient.java
+++ src/main/java/com/example/springboot/sample/with/s3/client/S3LocalClient.java
@@ -1,25 +1,29 @@
 package com.example.springboot.sample.with.s3.client;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.example.springboot.sample.with.s3.config.AwsCredentialConfig;
 import com.example.springboot.sample.with.s3.config.S3Config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("!local")
+@Profile("local")
 @Configuration
 @RequiredArgsConstructor
-public class S3Client {
+public class S3LocalClient {
 
     private final S3Config s3Config;
+    private final AwsCredentialConfig awsCredentialConfig;
 
     @Bean
     public AmazonS3 getS3Client() {
         return AmazonS3ClientBuilder
                 .standard()
                 .withRegion(s3Config.getRegion())
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentialConfig.getBasicAWSCredentials()))
                 .build();
     }
 }
